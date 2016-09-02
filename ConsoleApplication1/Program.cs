518605da3385af6197113f36afe21fa25171d17d
Anmol0823 <anmolratansh@vgy.se>
2016-09-02 14:24:30 +0200

    ÖVNING 1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace if_satser
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //ÖVNING 1
+            Console.WriteLine("Ange mönsterdjup på däck:");
+            double djup = double.Parse(Console.ReadLine());
+
+            if (djup < 1.6)
+                Console.WriteLine("Olagliga däck!");
+            else if (djup > 1.6)
+            
+                Console.WriteLine("Lagliga Däck!");
+
         }
     }
 }
+    
